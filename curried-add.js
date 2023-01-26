6c1ed7c7781215b9705c3cc3aeaa0ebbfd048404
Shaheen Khan <fshaheenkhan@gmail.com>
2023-01-26 17:04:51 -0500

    modified code

--- curried-add.js
+++ curried-add.js
@@ -1,3 +1,16 @@
+/**
+Write a function called curriedAdd. If you give this function a number, it returns a new function to you. If you give this function no arguments, it returns the total of all the numbers you’ve passed to it so far.
+
+let firstAdder = curriedAdd();
+let secondAdder = curriedAdd();
+let thirdAdder = curriedAdd();
+
+firstAdder(); // 0
+secondAdder(1)(2)(); // 3
+thirdAdder(2)(8)(5)(1)(); // 16} total 
+ 
+ */
+
 function curriedAdd(total) {
   if (total === undefined) return 0;
   return function addNext(num) {
